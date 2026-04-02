300927bc9a4005df554ab80faa942c509c2130ff
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2026-04-02 16:49:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-2xl shadow-xl text-center">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">
+          Tailwind Setup 🚀
+        </h1>
+
+        <p className="text-red-600 mb-6">
+          React + Vite + Tailwind is working perfectly!
+        </p>
+
+        <button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+          Click Me
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
